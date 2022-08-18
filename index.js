05768e244afb508643e1d04b32abc9701fc24604
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-18 19:04:25 +0300

    add file

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(x, y) {
+    return x + y;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function divide(x, y) {
+    return x / y;
+}
+function increment(n) {
+    n++;
+    return n; 
+}
+function decrement(n) {
+    n--;
+    return n;
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
